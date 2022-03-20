75fa2a2f53593b8cc4e8e354df87675cd0e25031
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-20 17:15:08 +0530

    Update diamond.js

--- js/diamond.js
+++ js/diamond.js
@@ -113,22 +113,8 @@ client.guildSchema = require('../schema/guild.js');
 client.userSchema = require('../schema/user.js');
 */
 /*
- * Quickmongo
- */
-const {
-	Database
-} = require('quickmongo');
-const db = new Database(client.config.bot.mongo);
-db.on('ready', () => {
-	console.log("Quickmongo Database Connected!");
-});
-(async () => {
-	await db.connect();
-})();
-/*
  * Client init variables
  */
-client.db = db;
 /*
  * Variables
  */
