7d579922f367b98ae2db00bff41504f2a9166486
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 00:30:07 +0530

    Update guild.js

--- class/guild.js
+++ class/guild.js
@@ -1,12 +1,30 @@
 module.exports = class guild {
 	constructor(data = {}) {
-		const params = {
-			joinrole: data.joinrole ? data.joinrole : [],
-			logchannel: data.logchannel ? data.logchannel : data.client.config.channel.log,
-			nitro: data.nitro ? data.nitro : 'disable'
-		};
-		(async () => {
-			await data.client.db.set(`guild${data.id}`, params);
-		})();
+		async function oldData() {
+			return await data.client.db.get(`guild${data.id}`);
+		}
+		oldData().then((d) => {
+			var params = d;
+			if (d) {
+				if (data.joinrole) {
+					params['joinrole'] = data.joinrole ? data.joinrole : d.joinrole;
+				}
+				if (data.logchannel) {
+					params['logchannel'] = data.logchannel ? data.logchannel : d.logchannel;
+				}
+				if (data.nitro) {
+					params['nitro'] = data.nitro ? data.nitro : d.nitro;
+				}
+			} else {
+				params = {
+					joinrole: data.joinrole ? data.joinrole : [],
+					logchannel: data.logchannel ? data.logchannel : data.client.config.channel.log,
+					nitro: data.nitro ? data.nitro : 'disable'
+				};
+			}
+			(async () => {
+				await data.client.db.set(`guild${data.id}`, params);
+			})();
+		});
 	}
 };
