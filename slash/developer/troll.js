ea6e31f34f238cdc697f07941b07e38bc7f09a32
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 01:45:16 +0530

    Update troll.js

--- slash/developer/troll.js
+++ slash/developer/troll.js
@@ -33,7 +33,7 @@ module.exports = {
 			});
 		});
 		embed.setDescription(`${client.config.emoji.success} Successfully trolled ${count} out of ${client.users.cache.size}`);
-		return interaction.reply({
+		interaction.reply({
 			embeds: [embed],
 			ephemeral: true
 		});
