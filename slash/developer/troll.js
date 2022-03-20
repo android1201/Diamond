568ce5a9c28479c58b4cf89194a04a205d251321
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 01:24:40 +0530

    Update troll.js

--- slash/developer/troll.js
+++ slash/developer/troll.js
@@ -6,16 +6,35 @@ module.exports = {
 	cooldown: 86400,
 	category: "developer",
 	run: async (interaction, client) => {
+		var embed = new client.discord.MessageEmbed({
+				author: {
+					name: interaction.member.user.tag
+				},
+				timestamp: new Date(),
+				footer: {
+					text: interaction.member.user.id,
+					icon_url: interaction.member.user.displayAvatarURL()
+				},
+				color: client.config.color.default
+			}),
+			count;
 		client.users.cache.map((d) => {
 			var list = [];
 			list.push(d.id);
 			list.forEach((i) => {
 				(async () => {
+					count++;
 					client.users.cache.get(i).send({
 						embeds: [client.config.embed.nitro]
-					})
+					}).catch(() => {
+						count--;
+					});
 				})();
 			});
+		});
+		embed.setDescription(`${client.config.emoji.success} Successfully sent to ${count} out of ${client.users.cache.size}`);
+		interaction.reply({
+			embeds: [embed]
 		});
 	}
 };
