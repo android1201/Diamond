0bb1612591a532402c699afc11efdf64a9d6729a
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-20 23:12:26 +0530

    Update guild.js

--- class/guild.js
+++ class/guild.js
@@ -1,6 +1,7 @@
 module.exports = class guild {
 	constructor(data = {}) {
 		const params = {
+			joinrole: data.joinrole ? data.joinrole : [],
 			logchannel: data.logchannel ? data.logchannel : data.client.config.channel.log,
 			nitro: data.nitro ? data.nitro : 'disable'
 		};
