9e3163b65c3147e7333ce773ab68fd6e5994f5a6
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-20 18:25:41 +0530

    Create quickmongo.js

---
+++ handlers/quickmongo.js
@@ -0,0 +1,18 @@
+module.exports = (
+	client
+) => {
+	/*
+	 * Quickmongo
+	 */
+	const {
+		Database
+	} = require('quickmongo');
+	const db = new Database(client.config.bot.mongo);
+	db.on('ready', () => {
+		console.log("Quickmongo Database Connected!");
+	});
+	(async () => {
+		await db.connect();
+	})();
+	client.db = db;
+};
