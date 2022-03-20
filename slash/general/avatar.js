20fb771f76e8b3c9986c3bc30a2f5937eac9cc66
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 03:44:51 +0530

    Create avatar.js

---
+++ slash/general/avatar.js
@@ -0,0 +1,36 @@
+module.exports = {
+	name: 'avatar',
+	description: 'Get user or own avatar.',
+	options: [{
+		name: 'user',
+		description: 'User to get avatar',
+		type: 6,
+	}],
+	category: 'general',
+	run: async (interaction) => {
+		var member = interaction.options.getMember('user') || interaction.member,
+			embed = new client.discord.MessageEmbed({
+				author: {
+					name: interaction.member.user.tag,
+					url: member.user.displayAvatarURL({
+						dynamic: true,
+						size: 4096
+					})
+				},
+				timestamp: new Date(),
+				footer: {
+					text: interaction.member.user.id,
+					icon_url: interaction.member.user.displayAvatarURL()
+				},
+				image: {
+					url: member.user.displayAvatarURL({
+						dynamic: true,
+						size: 4096
+					})
+				}
+			});
+		interaction.reply({
+			embeds: [embed]
+		});
+	}
+};
