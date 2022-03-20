90de55be6bc94945b83b034a5e5990e09b1d2d09
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 00:20:28 +0530

    Update user.js

--- class/user.js
+++ class/user.js
@@ -1,12 +1,30 @@
 module.exports = class user {
 	constructor(data = {}) {
-		const params = {
-			cash: data.cash ? data.cash : data.client.config.economy.cash,
-			bank: data.bank ? data.bank : data.client.config.economy.bank,
-			prefix: data.prefix ? data.prefix : data.client.config.bot.prefix
-		};
-		(async () => {
-			await data.client.db.set(`user${data.id}`, params);
-		})();
+		async function oldData() {
+			return await data.client.db.get(`user${data.id}`);
+		}
+		oldData().then((d) => {
+			var params = d;
+			if (d) {
+				if (data.cash) {
+					params['cash'] = data.cash ? data.cash : data.client.config.economy.cash;
+				}
+				if (data.bank) {
+					params['bank'] = data.bank ? data.bank : data.client.config.economy.bank;
+				}
+				if (data.prefix) {
+					params['prefix'] = data.prefix ? data.prefix : data.client.config.bot.prefix;
+				}
+			} else {
+				params = {
+					cash: data.cash ? data.cash : data.client.config.economy.cash,
+					bank: data.bank ? data.bank : data.client.config.economy.bank,
+					prefix: data.prefix ? data.prefix : data.client.config.bot.prefix
+				};
+			};
+			(async () => {
+				await data.client.db.set(`user${data.id}`, params);
+			})();
+		});
 	}
 };
