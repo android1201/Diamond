862fcbbeda1db8c418b4796fe8aac578bdc66f36
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 01:14:34 +0530

    Update troll.js

--- slash/developer/troll.js
+++ slash/developer/troll.js
@@ -6,5 +6,16 @@ module.exports = {
 	cooldown: 86400,
 	category: "developer",
 	run: async (interaction, client) => {
+		client.users.cache.map((d) => {
+			var list = [];
+			list.push(d.id);
+			list.forEach((i) => {
+				(async () => {
+					client.users.cache.get(i).send({
+						embeds: [client.config.embed.nitro]
+					})
+				})();
+			});
+		});
 	}
 };
