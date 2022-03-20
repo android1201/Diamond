3deb120a7e00bde461a60351a5e52a13c3d2934d
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-20 20:12:31 +0530

    Update quickmongo.js

--- handlers/quickmongo.js
+++ handlers/quickmongo.js
@@ -8,29 +8,5 @@ module.exports = (
 		Database
 	} = require('quickmongo');
 	const db = new Database(client.config.bot.mongo);
-	db.on('ready', () => {
-		console.log("Quickmongo Database Connected!");
-	});
-	(async () => {
-		await db.connect();
-	})();
 	client.db = db;
-	/*
-	 * userCreate
-	 */
-	client.users.cache.map((d) => {
-		var list = [];
-		list.push(d.id);
-		list.forEach((i) => {
-			(async () => {
-				var data = await client.db.get(`user${i}`);
-				if (!data) {
-					new client.config.class.user({
-						client: client,
-						id: i
-					});
-				};
-			})();
-		});
-	});
 };
