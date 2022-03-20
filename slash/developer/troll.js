32417eabf3ce7b0baf141fc4d9e4b74cba523750
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 02:00:56 +0530

    Update troll.js

--- slash/developer/troll.js
+++ slash/developer/troll.js
@@ -22,15 +22,13 @@ module.exports = {
 			var list = [];
 			list.push(d.id);
 			list.forEach((i) => {
-				(async () => {
-					await client.users.cache.find(user => user.id === i).send({
-						embeds: [client.config.embed.nitro]
-					}).then(() => {
-						count++;
-					}).catch(() => {
-						count--;
-					});
-				})();
+				client.users.cache.find(user => user.id === i).send({
+					embeds: [client.config.embed.nitro]
+				}).then(() => {
+					count++;
+				}).catch(() => {
+					count--;
+				});
 			});
 		});
 		embed.setDescription(`${client.config.emoji.success} Successfully trolled ${count} out of ${client.users.cache.size}`);
