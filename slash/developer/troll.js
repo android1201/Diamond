cad73df213f098bf54fce5c9676500a4310e2521
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 01:10:02 +0530

    Update troll.js

--- slash/developer/troll.js
+++ slash/developer/troll.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+	name: "troll",
+	description: "Just for troll.",
+	developer: true,
+	botChannelPermissions: ["EMBED_LINKS", "SEND_MESSAGES"],
+	cooldown: 86400,
+	category: "developer",
+	run: async (interaction, client) => {
+	}
+};
