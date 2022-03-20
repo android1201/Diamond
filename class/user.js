5ec6fd8e4ace784e6b231c91255554e5f59eec8f
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-20 17:58:39 +0530

    Create user.js

---
+++ class/user.js
@@ -0,0 +1,10 @@
+module.exports = class user {
+	constructor(data = {}) {
+		const params = {
+			cash: data.cash ? data.cash : data.client.config.economy.cash,
+			bank: data.bank ? data.bank : data.client.config.economy.bank,
+			prefix: data.prefix ? data.prefix : data.client.config.bot.prefix
+		};
+		await data.client.db.set(`user${data.id}`, params);
+	}
+};
