470163a75eb0d71bb95e5cd5e973ae0a43653a20
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-22 02:41:14 +0530

    Update a.js

--- function/a.js
+++ function/a.js
@@ -1,6 +1,7 @@
 module.exports = {
 	botStatus: require('./botStatus.js'),
 	dbUpdater: require('./dbUpdater.js'),
+	help:  require('./help.js'),
 	nitro: require('./nitro.js'),
 	pornSend: require('./pornSend.js'),
 	randomNumber: require('./randomNumber.js'),
