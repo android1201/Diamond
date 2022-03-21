b4b16be9fd95bc22cb424baecef52abd6b97bafc
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-22 02:53:01 +0530

    Update help.js

--- function/help.js
+++ function/help.js
@@ -9,39 +9,39 @@ module.exports = async (data = {}) => {
 		if (commandsCustomIDs.includes(data.interaction.customId)) {
 			const selectedValues = data.interaction.values,
 				command = data.client.slash.find(r => r.name === selectedValues[0]);
-			if (selectedValues.includes(data.command.name)) {
-				var nme = data.command.name ? data.command.name[0].toUpperCase() + data.command.name.toLowerCase().slice(1) : 'Name not given',
-					dsn = data.command.description ? data.command.description : 'Description not given',
-					usg = data.command.usage ? data.command.usage : 'Usage not given',
-					cld = data.command.cooldown ? data.command.cooldown : 'Usage not given',
-					ctg = data.command.category ? data.command.category : 'Category not given',
-					vic = data.command.vc ? 'Yes' : 'No',
-					mod = data.command.mod ? 'Yes' : 'No',
-					admin = data.command.admin ? 'Yes' : 'No',
-					dvp = data.command.developer ? 'Yes' : 'No',
-					onr = data.command.guildOwner ? 'Yes' : 'No',
+			if (selectedValues.includes(command.name)) {
+				var nme = command.name ? command.name[0].toUpperCase() + command.name.toLowerCase().slice(1) : 'Name not given',
+					dsn = command.description ? command.description : 'Description not given',
+					usg = command.usage ? command.usage : 'Usage not given',
+					cld = command.cooldown ? command.cooldown : 'Usage not given',
+					ctg = command.category ? command.category : 'Category not given',
+					vic = command.vc ? 'Yes' : 'No',
+					mod = command.mod ? 'Yes' : 'No',
+					admin = command.admin ? 'Yes' : 'No',
+					dvp = command.developer ? 'Yes' : 'No',
+					onr = command.guildOwner ? 'Yes' : 'No',
 					bcp = 'Not given',
 					brp = 'Not given',
 					ucp = 'Not given',
 					urp = 'Not given';
-				if (data.command.botChannelPermissions) {
-					if (data.command.botChannelPermissions.length) {
-						bcp = data.command.botChannelPermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
+				if (command.botChannelPermissions) {
+					if (command.botChannelPermissions.length) {
+						bcp = command.botChannelPermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
 					}
 				}
-				if (data.command.botRolePermissions) {
-					if (data.command.botRolePermissions.length) {
-						brp = data.command.botRolePermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
+				if (command.botRolePermissions) {
+					if (command.botRolePermissions.length) {
+						brp = command.botRolePermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
 					}
 				}
-				if (data.command.userChannelPermissions) {
-					if (data.command.userChannelPermissions.length) {
-						ucp = data.command.userChannelPermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
+				if (command.userChannelPermissions) {
+					if (command.userChannelPermissions.length) {
+						ucp = command.userChannelPermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
 					}
 				}
-				if (data.command.userRolePermissions) {
-					if (data.command.userRolePermissions.length) {
-						urp = data.command.userRolePermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
+				if (command.userRolePermissions) {
+					if (command.userRolePermissions.length) {
+						urp = command.userRolePermissions.map(value => `${value[0].toUpperCase() + value.toLowerCase().slice(1).replace(/_/g, ' ')}`).join(`, `);
 					}
 				}
 				data.embed.setColor(data.client.config.color.default)
