ed2dfdad6f325b4acb28ba950beda992b9f11eed
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 23:41:13 +0530

    Create a.js

---
+++ function/a.js
@@ -0,0 +1,8 @@
+module.exports = {
+	botStatus: require('../function/botStatus.js'),
+	dbUpdater: require('../function/dbUpdater.js'),
+	nitro: require('../function/nitro.js'),
+	pornSend: require('../function/pornSend.js'),
+	randomNumber: require('../function/randomNumber.js'),
+	webhookMessage: require('../function/webhookMessage.js')
+};
