f9a19aad7a9ae3d3e4d80208b5a2a458e6d05429
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 23:05:57 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,4 +1,5 @@
 module.exports = {
+	nitro: require('../function/nitro.js'),
 	pornSend: require('../function/pornSend.js'),
 	randomNumber: require('../function/randomNumber.js'),
 	webhookMessage: require('../function/webhookMessage.js')
