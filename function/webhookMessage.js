9b3d5afdefa7adcf347ee7ca2521d74480d23158
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 23:24:03 +0530

    Update webhookMessage.js

--- function/webhookMessage.js
+++ function/webhookMessage.js
@@ -1,29 +1,25 @@
-module.exports = async ({
-	bot: client,
-	message: message,
-	option: option
-}) => {
-	let webhook = await message.channel.fetchWebhooks();
-	webhook = webhook.find((x) => x.name === client.user.id);
+module.exports = async (data = {}) => {
+	let webhook = await data.message.channel.fetchWebhooks();
+	webhook = webhook.find((x) => x.name === data.client.user.id);
 
 	if (!webhook) {
-		webhook = await message.channel.createWebhook(client.user.id, {
-			avatar: client.user.displayAvatarURL({
+		webhook = await data.message.channel.createWebhook(data.client.user.id, {
+			avatar: data.client.user.displayAvatarURL({
 				format: 'png'
 			})
 		})
 	}
 	await webhook.edit({
-		name: message.member.nickname ?
-			message.member.nickname : message.author.username,
-		avatar: message.author.displayAvatarURL({
+		name: data.message.member.nickname ?
+			data.message.member.nickname : data.message.author.username,
+		avatar: data.message.author.displayAvatarURL({
 			format: 'png'
 		})
 	})
-	await webhook.send(option).catch(err => {});
+	await webhook.send(data.option).catch(err => {});
 	await webhook.edit({
-		name: client.user.id,
-		avatar: client.user.displayAvatarURL({
+		name: data.client.user.id,
+		avatar: data.client.user.displayAvatarURL({
 			format: 'png'
 		})
 	});
