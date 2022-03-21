1d1806899af0d53ea9b3e24b54b662785981e342
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 23:22:18 +0530

    Update randomNumber.js

--- function/randomNumber.js
+++ function/randomNumber.js
@@ -1,5 +1,5 @@
-module.exports = async (min, max) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
+module.exports = async (data = {}) => {
+	min = Math.ceil(data.min);
+	max = Math.floor(data.max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
