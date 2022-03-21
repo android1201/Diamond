d592040cb2a7bda8c45d3da1a99fb2614c3837f2
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-22 03:18:22 +0530

    Update help.js

--- function/help.js
+++ function/help.js
@@ -45,7 +45,7 @@ module.exports = async (data = {}) => {
 					}
 				}
 				data.embed.setColor(data.client.config.color.default)
-					.setDescription(`\`\`\`\nName: ${nme}\nDescription: ${dsn}\nUsage: ${usg}\nCooldown: ${cld}\nCategory: ${ctg}\nPermissions:\n	BCP: ${bcp}\n	BRP: ${brp}\n	UCP: ${ucp}\n	URP: ${urp}\n	Admin: ${admin}\n	Mod: ${mod}\n	Owner: ${onr}\n	Developer: ${dvp}\nTypes:\n	Vc: ${vic}\`\`\``);
+					.setDescription(`\`\`\`{data.client.config.emoji.info} General:\n	Name: ${nme}\n	Description: ${dsn}\n	Usage: ${usg}\n	Cooldown: ${cld}\n	Category: ${ctg}\n{data.client.config.emoji.access} Permissions:\n	BCP: ${bcp}\n	BRP: ${brp}\n	UCP: ${ucp}\n	URP: ${urp}\n	Admin: ${admin}\n	Mod: ${mod}\n	Owner: ${onr}\n	Developer: ${dvp}\n{data.client.config.emoji.types} Types:\n	Vc: ${vic}\`\`\``);
 				data.interaction.reply({
 					embeds: [data.embed],
 					ephemeral: true
