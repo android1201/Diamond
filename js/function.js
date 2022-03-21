6202e4a3834fafbbfdd19f531861e6c06733da6a
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-21 23:40:37 +0530

    Delete function.js

--- js/function.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	dbUpdater: require('../function/dbUpdater.js'),
-	nitro: require('../function/nitro.js'),
-	pornSend: require('../function/pornSend.js'),
-	randomNumber: require('../function/randomNumber.js'),
-	webhookMessage: require('../function/webhookMessage.js')
-};
