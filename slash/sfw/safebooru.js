828e005f59ac998f9ad7ffc4f3484bb418b05383
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 02:31:35 +0530

    Update safebooru.js

--- slash/sfw/safebooru.js
+++ slash/sfw/safebooru.js
@@ -16,7 +16,7 @@ module.exports = {
 				icon_url: interaction.member.user.displayAvatarURL()
 			}
 		});
-		client.porn.porn1.sfw.safebooru().then((i) => {
+		client.porn.porn2.sfw.safebooru().then((i) => {
 			embed.setImage(i.url)
 				.setColor(client.config.color.default);
 			return interaction.reply({
@@ -28,6 +28,10 @@ module.exports = {
 				.setDescription(`\`\`\`\n${client.config.emoji.error} Error while executing command!\`\`\``);
 			return interaction.reply({
 				embeds: [embed]
+			}).then(() => {
+				setTimeout(() => {
+					interaction.deleteReply().catch(() => {});
+				}, 5000);
 			});
 		});
 	}
