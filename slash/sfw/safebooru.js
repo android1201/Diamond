64f3e4b6aadd481ae6786cd6bec2523452313863
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 02:05:21 +0530

    Create safebooru.js

---
+++ slash/sfw/safebooru.js
@@ -0,0 +1,32 @@
+module.exports = {
+	name: "safebooru",
+	description: "Get random safebooru image.",
+	botChannelPermissions: ["EMBED_LINKS", "MANAGE_MESSAGES", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"],
+	cooldown: 5,
+	category: "image",
+	run: async (interaction, client) => {
+		var embed = new client.discord.MessageEmbed({
+			author: {
+				name: interaction.member.user.tag
+			},
+			timestamp: new Date(),
+			footer: {
+				text: interaction.member.user.id,
+				icon_url: interaction.member.user.displayAvatarURL()
+			}
+		});
+		client.porn.porn2.sfw.safebooru().then((i) => {
+			embed.setImage(i.url)
+				.setColor(client.config.color.default);
+			return interaction.reply({
+				embeds: [embed]
+			});
+		}).catch(() => {
+			embed.setColor(client.config.color.error)
+				.setDescription(`\`\`\`\n${client.config.emoji.error} Error while executing command!\`\`\``);
+			return interaction.reply({
+				embeds: [embed]
+			});
+		});
+	}
+};
