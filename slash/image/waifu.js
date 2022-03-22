7ad7dc8950818a551b94bac69673ca8f1482b4bc
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 02:25:08 +0530

    Update waifu.js

--- slash/image/waifu.js
+++ slash/image/waifu.js
@@ -15,6 +15,7 @@ module.exports = {
 				icon_url: interaction.member.user.displayAvatarURL()
 			}
 		});
+console.log(client.porn.porn2.sfw);
 		client.porn.porn2.sfw.waifu().then((i) => {
 			embed.setImage(i.url)
 				.setColor(client.config.color.default);
