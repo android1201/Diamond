ec41f926078e38603cb21f9cd0a9497b608e826d
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-22 20:17:35 +0530

    Update help.js

--- function/help.js
+++ function/help.js
@@ -1,13 +1,13 @@
 module.exports = async (data = {}) => {
-	if (data.interaction.isSelectMenu()) {
+	if (data.channel.isSelectMenu()) {
 		var commandsCustomIDs = [],
 			fs = require('fs'),
 			path = require('path');
 		fs.readdirSync(path.resolve(__dirname, "../slash/")).map(async (dir) => {
 			commandsCustomIDs.push(dir);
 		});
-		if (commandsCustomIDs.includes(data.interaction.customId)) {
-			const selectedValues = data.interaction.values,
+		if (commandsCustomIDs.includes(data.channel.customId)) {
+			const selectedValues = data.channel.values,
 				command = data.client.slash.find(r => r.name === selectedValues[0]);
 			if (selectedValues.includes(command.name)) {
 				data.client.config.function.helpEmbed({
