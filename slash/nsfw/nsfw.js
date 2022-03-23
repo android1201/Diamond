9e2a0ff7a74913403ff19aca250c64fd9693d116
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 14:12:25 +0530

    Update nsfw.js

--- slash/nsfw/nsfw.js
+++ slash/nsfw/nsfw.js
@@ -11,149 +11,149 @@ module.exports = {
 		type: 3,
 		required: true,
 		choices: [{
-			name: 'Ahegao',
-			value: 'ahegao'
+			name: "Ahegao",
+			value: "ahegao"
 		}, {
-			name: 'Animated',
-			value: 'animated'
+			name: "Animated",
+			value: "animated"
 		}, {
-			name: 'Jav',
-			value: 'jav'
+			name: "Jav",
+			value: "jav"
 		}, {
-			name: 'Porngif',
-			value: 'porngif'
+			name: "Porngif",
+			value: "porngif"
 		}, {
-			name: 'Realbooru',
-			value: 'realbooru'
+			name: "Realbooru",
+			value: "realbooru"
 		}, {
-			name: 'Blowjob',
-			value: 'blowjob'
+			name: "Blowjob",
+			value: "blowjob"
 		}, {
-			name: 'Missionary',
-			value: 'missionary'
+			name: "Missionary",
+			value: "missionary"
 		}, {
-			name: 'Cowgirl',
-			value: 'cowgirl'
+			name: "Cowgirl",
+			value: "cowgirl"
 		}, {
-			name: 'Doggy_style',
-			value: 'doggy_style'
+			name: "Doggystyle",
+			value: "doggystyle"
 		}, {
-			name: 'Slut',
-			value: 'slut'
+			name: "Slut",
+			value: "slut"
 		}, {
-			name: 'Porn_star',
-			value: 'porn_star'
+			name: "Pornstar",
+			value: "pornstar"
 		}, {
-			name: 'Anal',
-			value: 'anal'
+			name: "Anal",
+			value: "anal"
 		}, {
-			name: 'Anal_penetration',
-			value: 'anal_penetration'
+			name: "Analpenetration",
+			value: "analpenetration"
 		}, {
-			name: 'Areolae',
-			value: 'areolae'
+			name: "Areolae",
+			value: "areolae"
 		}, {
-			name: 'Asian',
-			value: 'asian'
+			name: "Asian",
+			value: "asian"
 		}, {
-			name: 'Ass',
-			value: 'ass'
+			name: "Ass",
+			value: "ass"
 		}, {
-			name: 'Big_ass',
-			value: 'big_ass'
+			name: "Bigass",
+			value: "bigass"
 		}, {
-			name: 'Bra',
-			value: 'bra'
+			name: "Bra",
+			value: "bra"
 		}, {
-			name: 'Brunette',
-			value: 'brunette'
+			name: "Brunette",
+			value: "brunette"
 		}, {
-			name: 'Bondage',
-			value: 'bondage'
+			name: "Bondage",
+			value: "bondage"
 		}, {
-			name: 'Cleavage',
-			value: 'cleavage'
+			name: "Cleavage",
+			value: "cleavage"
 		}, {
-			name: 'Cum_on_face',
-			value: 'cum_on_face'
+			name: "Cumonface",
+			value: "cumonface"
 		}, {
-			name: 'Cumshot',
-			value: 'cumshot'
+			name: "Cumshot",
+			value: "cumshot"
 		}, {
-			name: 'Doggystyle',
-			value: 'doggystyle'
+			name: "Doggystyle",
+			value: "doggystyle"
 		}, {
-			name: 'From_behind',
-			value: 'from_behind'
+			name: "Frombehind",
+			value: "frombehind"
 		}, {
-			name: 'Huge_breasts',
-			value: 'huge_breasts'
+			name: "Hugebreasts",
+			value: "hugebreasts"
 		}, {
-			name: 'Large_penis',
-			value: 'large_penis'
+			name: "Largepenis",
+			value: "largepenis"
 		}, {
-			name: 'Milf',
-			value: 'milf'
+			name: "Milf",
+			value: "milf"
 		}, {
-			name: 'Nipples',
-			value: 'nipples'
+			name: "Nipples",
+			value: "nipples"
 		}, {
-			name: 'Nude',
-			value: 'nude'
+			name: "Nude",
+			value: "nude"
 		}, {
-			name: 'Oral',
-			value: 'oral'
+			name: "Oral",
+			value: "oral"
 		}, {
-			name: 'Open_mouth',
-			value: 'open_mouth'
+			name: "Openmouth",
+			value: "openmouth"
 		}, {
-			name: 'Brazzers',
-			value: 'brazzers'
+			name: "Brazzers",
+			value: "brazzers"
 		}, {
-			name: 'Panties',
-			value: 'panties'
+			name: "Panties",
+			value: "panties"
 		}, {
-			name: 'Penetration',
-			value: 'penetration'
+			name: "Penetration",
+			value: "penetration"
 		}, {
-			name: 'Penis',
-			value: 'penis'
+			name: "Penis",
+			value: "penis"
 		}, {
-			name: 'Removing_panties',
-			value: 'removing_panties'
+			name: "Removingpanties",
+			value: "removingpanties"
 		}, {
-			name: 'Sexy',
-			value: 'sexy'
+			name: "Sexy",
+			value: "sexy"
 		}, {
-			name: 'Sideboob',
-			value: 'sideboob'
+			name: "Sideboob",
+			value: "sideboob"
 		}, {
-			name: 'Stockings',
-			value: 'stockings'
+			name: "Stockings",
+			value: "stockings"
 		}, {
-			name: 'Balls',
-			value: 'balls'
+			name: "Balls",
+			value: "balls"
 		}, {
-			name: 'Handjob',
-			value: 'handjob'
+			name: "Handjob",
+			value: "handjob"
 		}, {
-			name: 'Oil',
-			value: 'oil'
+			name: "Oil",
+			value: "oil"
 		}, {
-			name: 'Sucking_balls',
-			value: 'sucking_balls'
+			name: "Suckingballs",
+			value: "suckingballs"
 		}, {
-			name: 'Bdsm',
-			value: 'bdsm'
+			name: "Bdsm",
+			value: "bdsm"
 		}, {
-			name: 'Finger_in_mouth',
-			value: 'finger_in_mouth'
+			name: "Fingerinmouth",
+			value: "fingerinmouth"
 		}, {
-			name: 'Deepthroat',
-			value: 'deepthroat'
+			name: "Deepthroat",
+			value: "deepthroat"
 		}, {
-			name: 'Bbc',
-			value: 'bbc'
+			name: "Bbc",
+			value: "bbc"
 		}]
 	},
 	run: async (interaction, client) => {
