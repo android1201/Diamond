60c91ccbc2fd8d289f3152f97e78e9dcdc28aa5f
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 16:16:30 +0530

    Update a.js

--- function/a.js
+++ function/a.js
@@ -7,5 +7,6 @@ module.exports = {
 	nsfwSender: require('./nsfwSender.js'),
 	pornSend: require('./pornSend.js'),
 	randomNumber: require('./randomNumber.js'),
+	sfwSender: require('./sfwSender.js'),
 	webhookMessage: require('./webhookMessage.js')
 };
