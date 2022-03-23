61a0ac0f9af601fc7272009196931d2a52484365
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 14:45:14 +0530

    Create waifu.js

---
+++ slash/sfw/waifu.js
@@ -0,0 +1,51 @@
+module.exports = {
+	name: "waifu",
+	description: "Get waifu image.",
+	botChannelPermissions: ["EMBED_LINKS", "SEND_MESSAGES"],
+	cooldown: 5,
+	category: "sfw",
+	sfw: true,
+	run: async (interaction, client) => {
+		const type = interaction.options.getString('type');
+		client.porn.porn2.nsfw[waifu]().then((i) => {
+			var embed = new client.discord.MessageEmbed({
+				author: {
+					name: interaction.member.user.tag,
+					url: i.url
+				},
+				timestamp: new Date(),
+				footer: {
+					text: interaction.member.user.id,
+					icon_url: interaction.member.user.displayAvatarURL()
+				},
+				image: {
+					url: i.url
+				},
+				color: client.config.color.default
+			});
+			return interaction.reply({
+				embeds: [embed]
+			});
+		}).catch((e) => {
+			var embed = new client.discord.MessageEmbed({
+				author: {
+					name: interaction.member.user.tag
+				},
+				timestamp: new Date(),
+				footer: {
+					text: interaction.member.user.id,
+					icon_url: interaction.member.user.displayAvatarURL()
+				},
+				color: client.config.color.error,
+				description: `\`\`\`\n${client.config.emoji.error} Error while executing command!\`\`\``
+			});
+			return interaction.reply({
+				embeds: [embed]
+			}).then(() => {
+				setTimeout(() => {
+					interaction.deleteReply().catch(() => {});
+				}, 5000);
+			});
+		});
+	}
+};
