6bca71ae0d358a16a2507e7933589f9daae0b302
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 16:23:03 +0530

    Update nsfwSender.js

--- function/nsfwSender.js
+++ function/nsfwSender.js
@@ -5,8 +5,7 @@ module.exports = async (data = {}) => {
 		Object.keys(endpoints.nsfw).forEach(async (i) => {
 			iList.push(i);
 		});
-		var mList = Math.floor(Math.random * iList.length);
-console.log(mList);
+		const mList = iList[Math.floor(Math.random() * iList.length)];
 		data.client.porn.porn2.nsfw[mList]().then(i => {
 			var image = i.url ? i.url : i.image,
 				embed = {
