f1a1fb12116c21c510df6458abdd660cc72530a9
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 14:42:42 +0530

    Create sfw1.js

---
+++ slash/sfw/sfw1.js
@@ -0,0 +1,133 @@
+module.exports = {
+	name: "sfw",
+	description: "Get sfw image.",
+	botChannelPermissions: ["EMBED_LINKS", "SEND_MESSAGES"],
+	cooldown: 5,
+	category: "sfw",
+	sfw: true,
+	options: [{
+		name: "type",
+		description: "Type of sfw to show.",
+		type: 3,
+		required: true,
+		choices: [{
+			name: "Himiko",
+			value: "himiko"
+		}, {
+			name: "Kiara",
+			value: "kiara"
+		}, {
+			name: "Xuanzang",
+			value: "xuanzang"
+		}, {
+			name: "Bb",
+			value: "bb"
+		}, {
+			name: "Nero",
+			value: "nero"
+		}, {
+			name: "Atago",
+			value: "atago"
+		}, {
+			name: "Takao",
+			value: "takao"
+		}, {
+			name: "Bremerton",
+			value: "bremerton"
+		}, {
+			name: "St_louis",
+			value: "st_louis"
+		}, {
+			name: "Sirius",
+			value: "sirius"
+		}, {
+			name: "Belfast",
+			value: "belfast"
+		}, {
+			name: "Enterprise",
+			value: "enterprise"
+		}, {
+			name: "Taihou",
+			value: "taihou"
+		}, {
+			name: "Dido",
+			value: "dido"
+		}, {
+			name: "Hood",
+			value: "hood"
+		}, {
+			name: "Formidable",
+			value: "formidable"
+		}, {
+			name: "Jean",
+			value: "jean"
+		}, {
+			name: "Beidou",
+			value: "beidou"
+		}, {
+			name: "Mona",
+			value: "mona"
+		}, {
+			name: "Lumine",
+			value: "lumine"
+		}, {
+			name: "Lisa",
+			value: "lisa"
+		}, {
+			name: "Keqing",
+			value: "keqing"
+		}, {
+			name: "Barbara",
+			value: "barbara"
+		}, {
+			name: "Klee",
+			value: "klee"
+		}, {
+			name: "Amber",
+			value: "amber"
+		}]
+	}],
+	run: async (interaction, client) => {
+		const type = interaction.options.getString('type');
+		client.porn.porn2.nsfw[type]().then((i) => {
+			var embed = new client.discord.MessageEmbed({
+				author: {
+					name: interaction.member.user.tag,
+					url: i.url
+				},
+				timestamp: new Date(),
+				footer: {
+					text: interaction.member.user.id,
+					icon_url: interaction.member.user.displayAvatarURL()
+				},
+				image: {
+					url: i.url
+				},
+				color: client.config.color.default
+			});
+			return interaction.reply({
+				embeds: [embed]
+			});
+		}).catch((e) => {
+			var embed = new client.discord.MessageEmbed({
+				author: {
+					name: interaction.member.user.tag
+				},
+				timestamp: new Date(),
+				footer: {
+					text: interaction.member.user.id,
+					icon_url: interaction.member.user.displayAvatarURL()
+				},
+				color: client.config.color.error,
+				description: `\`\`\`\n${client.config.emoji.error} Error while executing command!\`\`\``
+			});
+			return interaction.reply({
+				embeds: [embed]
+			}).then(() => {
+				setTimeout(() => {
+					interaction.deleteReply().catch(() => {});
+				}, 5000);
+			});
+		});
+	}
+};
