1271dadcbb66728062255186f2f2881f97380f3d
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-23 16:05:07 +0530

    Update a.js

--- function/a.js
+++ function/a.js
@@ -4,6 +4,7 @@ module.exports = {
 	help:  require('./help.js'),
 	helpEmbed:  require('./helpEmbed.js'),
 	nitro: require('./nitro.js'),
+	nsfwSender: require('./nsfwSender.js'),
 	pornSend: require('./pornSend.js'),
 	randomNumber: require('./randomNumber.js'),
 	webhookMessage: require('./webhookMessage.js')
