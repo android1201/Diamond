6d0030b2d72ceb846efd23428698530fa51df74f
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-31 04:47:09 +0530

    Create play.js

---
+++ slash/music/play.js
@@ -0,0 +1,77 @@
+module.exports = {
+	name: 'play',
+	description: 'Searches for a song or playlist plays it in voice.',
+	botChannelPermissions: ["EMBED_LINKS", "SEND_MESSAGES"],
+	options: [{
+		name: 'search',
+		description: 'The song or playlist name that you want to search for.',
+		type: 3,
+		required: true
+	}, {
+		name: "top",
+		description: "Whether the song should be added to the top of the queue.",
+		type: 5,
+	}, {
+		name: "shuffle",
+		description: "Only applies to playlists. Shuffles the playlist when adding to the queue.",
+		type: 5,
+	}],
+	category: 'music',
+	cooldown: 5,
+	vc: true,
+	run: async (interaction, client) => {
+		const {
+			MessageEmbed
+		} = client.discord, {
+			QueryType
+		} = require('discord-player')
+		embed = new client.discord.MessageEmbed({
+				author: {
+					name: interaction.member.user.tag
+				},
+				color: client.config.color.default,
+				timestamp: new Date(),
+				footer: {
+					text: interaction.member.user.id,
+					icon_url: interaction.member.user.displayAvatarURL()
+				},
+			}),
+			voiceChannel = interaction.member.voice.channel,
+			query = interaction.options.getString('search'),
+			guild = interaction.guild,
+			channel = interaction.channel,
+			member = interaction.user;
+		await interaction.defer();
+		const searchResult = await client.player
+			.search(query, {
+				requestedBy: interaction.member,
+				searchEngine: QueryType.AUTO
+			})
+			.catch(() => {});
+		if (!searchResult || !searchResult.tracks.length) return interaction.reply({
+			content: 'No results were found!'
+		});
+
+		const queue = await client.player.createQueue(guild, {
+			ytdlOptions: {
+				filter: 'audioonly',
+				highWaterMark: 1 << 30,
+				dlChunkSize: 0,
+			},
+			metadata: channel
+		});
+		try {
+			if (!queue.connection) await queue.connect(member.voice.channel);
+		} catch {
+			void client.player.deleteQueue(interaction.guild.id);
+			return interaction.reply({
+				content: 'Could not join your voice channel!'
+			});
+		}
+		await interaction.reply({
+			content: `⏱ | Loading your ${searchResult.playlist ? 'playlist' : 'track'}...`
+		});
+		searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
+		if (!queue.playing) await queue.play();
+	},
+};
