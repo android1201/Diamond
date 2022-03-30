86a5d43f068ad115e62bc8808f4ad731c8b28083
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-31 03:04:24 +0530

    Create player.js

---
+++ handlers/player.js
@@ -0,0 +1,22 @@
+module.exports = async (client) => {
+	const {
+		Player
+	} = require("discord-music-player");
+	client.player = new Player(client, {
+		leaveOnEnd: true,
+		leaveOnStop: false,
+		leaveOnEmpty: true,
+		timeout: 600000
+	});
+	client.player
+		.on('channelEmpty', (queue) => {})
+		.on('songAdd', (queue, song) => {})
+		.on('playlistAdd', (queue, playlist) => {})
+		.on('queueDestroyed', (queue) => {})
+		.on('queueEnd', (queue) => {})
+		.on('songChanged', (queue, newSong, oldSong) => {})
+		.on('songFirst', (queue, song) => {})
+		.on('clientDisconnect', (queue) => {})
+		.on('clientUndeafen', (queue) => {})
+		.on('error', (error, queue) => {});
+};
