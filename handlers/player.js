8f709ad117db60c65cbb9fd58b7200827f4b868b
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-31 04:27:12 +0530

    Update player.js

--- handlers/player.js
+++ handlers/player.js
@@ -15,4 +15,19 @@ module.exports = async (client) => {
 			}
 		}
 	});
+	client.player.on("error", (queue, error) => {
+		console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+	}).on("connectionError", (queue, error) => {
+		console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+	}).on("trackStart", (queue, track) => {
+		queue.metadata.send(`🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+	}).on("trackAdd", (queue, track) => {
+		queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
+	}).on("botDisconnect", (queue) => {
+		queue.metadata.send("❌ | I was manually disconnected from the voice channel, clearing queue!");
+	}).on("channelEmpty", (queue) => {
+		queue.metadata.send("❌ | Nobody is in the voice channel, leaving...");
+	}).on("queueEnd", (queue) => {
+		queue.metadata.send("✅ | Queue finished!");
+	});
 };
