d72255166b83704fdffd9ae7b863d2bf772cbda2
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-31 04:09:45 +0530

    Delete isPlaylist.js

--- function/isPlaylist.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (search) => {
-	const {
-		Utils
-	} = require("discord-music-player");
-	return Utils.regexList.SpotifyPlaylist.test(search) ||
-		Utils.regexList.YouTubePlaylist.test(search) ||
-		Utils.regexList.ApplePlaylist.test(search);
-};
